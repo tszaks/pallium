e4bf2f326b05ac83ed915447cfcf2d60bbc0e288
Tyler Szakacs <tyler@szakacsmedia.com>
2026-07-04 21:13:02 -0400

    Prepare v0.9.4 release

--- npm/scripts/test.js
+++ npm/scripts/test.js
@@ -3,11 +3,16 @@
 
 const assert = require("node:assert/strict");
 const { assetName, parseChecksum, platformKey } = require("./lib");
+const packageJson = require("../package.json");
+
+const escapedVersion = packageJson.version.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+const expectedAsset = new RegExp(`^pallium_${escapedVersion}_(darwin|linux)_(arm64|amd64)\\.tar\\.gz$`);
+const checksumAsset = `pallium_${packageJson.version}_darwin_arm64.tar.gz`;
 
 assert.match(platformKey(), /^(darwin|linux|win32|freebsd|openbsd|aix|sunos)-/);
 if (["darwin-arm64", "darwin-amd64", "linux-arm64", "linux-amd64"].includes(platformKey())) {
-  assert.match(assetName(), /^pallium_0\.9\.3_(darwin|linux)_(arm64|amd64)\.tar\.gz$/);
+  assert.match(assetName(), expectedAsset);
 }
-assert.equal(parseChecksum("abc123  pallium_0.9.3_darwin_arm64.tar.gz\n", "pallium_0.9.3_darwin_arm64.tar.gz"), "abc123");
+assert.equal(parseChecksum(`abc123  ${checksumAsset}\n`, checksumAsset), "abc123");
 
 console.log("pallium npm wrapper tests passed");
